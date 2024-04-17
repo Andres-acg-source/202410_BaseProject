c2333b61ad3da338446dda47332f50270ba95e8b
Andrés Cáceres <a.caceresg@uniandes.edu.co>
2024-04-17 11:47:57 -0500

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BookModule } from './book/book.module';
+import { EditorialModule } from './editorial/editorial.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EditorialModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
